f1a277007be85ccb96671b2c47494636f2cdbd9d
Radosław Różański <r.rozanski@gmail.com>
2018-01-27 11:46:28 +0100

    Test touch events

--- client/js/src/scenes/scene-main.js
+++ client/js/src/scenes/scene-main.js
@@ -27,4 +27,30 @@ export function registerSceneMain() {
             });
         }
     });
+
+    AFRAME.registerComponent('test-sphere', {
+        init: function () {
+            console.log("!!!");
+
+            let test123 = document.getElementById("test-sphere");
+
+            test123.addEventListener('click', function () {
+                // test123.setAttribute('scale', {x: 2, y: 1, z: 2});
+                console.log("click");
+                test123.setAttribute('color', 'orange');
+            });
+
+            test123.addEventListener('mouseenter', function () {
+                // test123.setAttribute('scale', {x: 2, y: 1, z: 2});
+                console.log("mouseenter");
+                test123.setAttribute('color', '#24CAFF');
+            });
+
+            test123.addEventListener('mousedown', function () {
+                // test123.setAttribute('scale', {x: 2, y: 1, z: 2});
+                console.log("mousedown");
+                test123.setAttribute('color', '#EF2D5E');
+            });
+        }
+    });
 }
