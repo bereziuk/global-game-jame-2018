04459db6cda49606374bfadbfe7e194afa93c317
Adam Bereziuk <adam@bereziuk.com>
2018-01-27 20:39:57 +0100

    tape

--- client/js/src/app.js
+++ client/js/src/app.js
@@ -3,50 +3,54 @@ import {registerSceneMain} from "./scenes/scene-main";
 import { setTimeout } from "timers";
 
 $(document).ready(() => {
-  alert(1);
-  generateTape();
   registerClickDragComponent();
   registerSceneMain();
+  generateProducts();
 });
 
-function generateTape() {
-    console.log(tape);
-
+function generateProducts() {
     const parent = document.getElementById("tape");
 
-    createNewProduct(1, parent);
-    createNewProduct(2, parent);
-    createNewProduct(3, parent);
-    createNewProduct(4, parent);
-    createNewProduct(5, parent);
-    createNewProduct(6, parent);
-    createNewProduct(7, parent);
-    createNewProduct(8, parent);
-    createNewProduct(9, parent);
-    createNewProduct(10, parent);
-    createNewProduct(11, parent);
-
-    setTimeout();
-}
+    setTimeout(() => {
+      createNewProduct(1, parent);
+    }, 5000);
+  }
 
 function createNewProduct(index, parent) {
-    const entity = document.createElement("a-entity");
+    const product = document.createElement("a-box");
+    const boxHorizontalPosition = "0 1.5 0";
 
-    const boxHorizontal = document.createElement("a-box");
-    const boxHorizontalPosition = (0.2 * index).toString() + " 0 0";
-    boxHorizontal.setAttribute("position", boxHorizontalPosition);
-    boxHorizontal.setAttribute("dynamic-body", { shape: "box", mass: 0 });
-    boxHorizontal.setAttribute("material", { src: "#texture-tape" });
-    boxHorizontal.setAttribute("scale", "0.2 0.05");
+    product.classList.add("product");
+    product.setAttribute("position", boxHorizontalPosition);
+    product.setAttribute("dynamic-body", { shape: "box", mass: 0.01 });
+    product.setAttribute("material", { color: "blue" });
+    product.setAttribute("scale", "0.2 0.2 0.2");
 
-    const boxVerticalPosition = (((0.2 * index) + 0.08)) + " 0.06 0";
-    const boxVertical = document.createElement("a-box");
-    boxVertical.setAttribute("position", boxVerticalPosition);
-    boxVertical.setAttribute("dynamic-body", { shape: "box", mass: 0 });
-    boxVertical.setAttribute("material", { opacity: 0 });
-    boxVertical.setAttribute("scale", "0.02 0.1 1");
+    product.addEventListener('collide', function (e) {
+      const elId = e.detail.body.el.id;
+      const tapeId = "tape-box";
+      console.log(elId);
 
-    entity.appendChild(boxHorizontal);
-    entity.appendChild(boxVertical);
-    parent.appendChild(entity);
+      if (elId === tapeId) {
+        // setTimeout(() => {
+          const elPos = product.getAttribute("position");
+          console.log(elPos.x);
+
+
+          product.applyImpulse(
+            new CANNON.Vec3(-1, 1, 0),
+            new CANNON.Vec3().copy(product.getComputedAttribute('position'))
+          );
+
+          // product.pause();
+          // product.setAttribute("position", "x", elPos.x - 1.2);
+          // product.setAttribute("position", "y", elPos.y + 0.05);
+          // product.play();
+        // }, 0);
+      }
+  });
+
+    parent.appendChild(product);
 }
+
+
